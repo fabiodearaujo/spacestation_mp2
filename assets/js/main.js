b3a7b71fe4a67c39d0307868d71375b97158bec4
Fabio De Araujo <fabioaraujo76@gmail.com>
2020-06-07 23:07:55 +0100

    added custom marker into the map pointing to the positioning received from the API

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,15 +22,11 @@ $(function() {
     })
   });
 
-// URL FOR API ISS LOCATION
-  let urlISS = "https://api.wheretheiss.at/v1/satellites/25544";
-
 
 //   LEAFLET JS MAP SPACE CREATION
   const mymap = L.map("worldMap").setView([0, 0], 1);
 
    //   ADDING TILES TO THE MAP FROM MAPBOX.COM
-
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiaW9kZWFyYXVqbyIsImEiOiJja2I1aTkzcjcwcG50MnlwYml4Z2kyM3dpIn0.psit76bCm1AtS-ffHfX1Uw', {
     maxZoom: 18,
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
@@ -39,6 +35,18 @@ $(function() {
     zoomOffset: -1
 }).addTo(mymap);
 
+// CREATING A CUSTOM MARKER TO THE MAP
+const issIcon = L.icon( {
+    iconUrl: '/assets/img/iss200px.png',
+    iconSize: [40,30],
+    iconAnchor: [25,16],
+})
+
+let myMarker = L.marker([0,0], {icon: issIcon}).addTo(mymap);
+
+
+// URL FOR API ISS LOCATION
+let urlISS = "https://api.wheretheiss.at/v1/satellites/25544";
 
 //   FETCHING THE DATA FROM JSON API - EXAMPLE CODE FROM dcode YOUTUBE CHANNEL - https://www.youtube.com/watch?v=5VCY9yCZnlc
 function getIss() {
@@ -47,10 +55,18 @@ function getIss() {
         return response.json();
 
     }).then(function(issData) {
+        let latData = issData.latitude;
+        let longData = issData.longitude;
+
+        // DISPLAY LATITUDE AND LONGITUDE ON THE PAGE 
+        $("#latValue").html(latData);
+        $("#longValue").html(longData);
+
+        // INCLUDE THE MARKER ON THE MAP
+        marker.setLatLng([latData, longData]);
+
+
         
-        $("#latValue").html(issData.latitude);
-        $("#longValue").html(issData.longitude);
-       
     }).catch(function(error) {
         
         console.log("Something went wrong with retrieving data, please try again later");
