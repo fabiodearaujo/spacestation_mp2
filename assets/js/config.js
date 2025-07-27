3582747af2a9e4b226f942c5888341c0aab4b1e3
Fabio De Araujo <52252478+fabiodearaujo@users.noreply.github.com>
2025-07-27 21:56:13 +0200

    removed server and moved to its own repo and updated API Call (#13)

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     apiBaseUrl: window.location.hostname === 'fabiodearaujo.github.io'
-        ? 'https://spacestation-mp2.vercel.app' // Replace with your actual Render URL
+        ? 'https://spacestation-api.vercel.app'
         : 'http://localhost:3000'
 };
